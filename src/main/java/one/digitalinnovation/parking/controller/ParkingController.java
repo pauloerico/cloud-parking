c349a37ddd21b589c857b698f956f5fd999ff5fd
Paulo <deoliveira.pe@gmail.com>
2022-10-31 01:27:04 -0300

    feat: Implement Parking Controller

--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -1,2 +1,32 @@
-package one.digitalinnovation.parking.controller;public class ParkingController {
+package one.digitalinnovation.parking.controller;
+
+import one.digitalinnovation.parking.controller.dto.ParkingDTO;
+import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
+import one.digitalinnovation.parking.model.Parking;
+import one.digitalinnovation.parking.service.ParkingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ParkingDTO>> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return ResponseEntity.ok(result);
+    }
 }
