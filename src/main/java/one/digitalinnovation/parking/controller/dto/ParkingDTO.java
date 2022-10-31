8a24b287c5cec111d0c93bf9418325f18e9cc22d
Paulo <deoliveira.pe@gmail.com>
2022-10-31 01:27:48 -0300

    feat: Implement Parking DTO

--- src/main/java/one/digitalinnovation/parking/controller/dto/ParkingDTO.java
+++ src/main/java/one/digitalinnovation/parking/controller/dto/ParkingDTO.java
@@ -1,4 +1,23 @@
 package one.digitalinnovation.parking.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ParkingDTO {
+
+    private String id;
+    private String license;
+    private String state;
+    private String model;
+    private String color;
+    private LocalDateTime entryDate;
+    private LocalDateTime exitDate;
+    private Double bill;
+
+    public String getId() {
+        return id;
+    }
 }
