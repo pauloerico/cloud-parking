b0e88f029c0f8c7a015efe927bbc68df1ea94f5e
Paulo <deoliveira.pe@gmail.com>
2022-10-31 01:24:56 -0300

    feat: Implement Parking Service

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -1,2 +1,26 @@
-package one.digitalinnovation.parking.service;public class ParkingService {
+package one.digitalinnovation.parking.service;
+
+import one.digitalinnovation.parking.model.Parking;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class ParkingService {
+    private static Map<String, Parking> parkingMap = new HashMap();
+
+    static {
+        var id = getUUID();
+        Parking parking = new Parking(id, "OMS-1111", "SC", "CELtA", "PRETO");
+        parkingMap.put(id, parking);
+    }
+
+    public List<Parking> findAll(){
+        return parkingMap.values().stream().collect(Collectors.toList());
+    }
+
+    private static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
 }
