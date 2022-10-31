8560e9cc263cea2d2cbc28948041de2f20886876
Paulo <deoliveira.pe@gmail.com>
2022-10-31 01:25:50 -0300

    feat: Implement Parking Model

--- src/main/java/one/digitalinnovation/parking/model/Parking.java
+++ src/main/java/one/digitalinnovation/parking/model/Parking.java
@@ -1,2 +1,27 @@
-package one.digitalinnovation.parking.model;public class Parking {
+package one.digitalinnovation.parking.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Parking {
+    @NonNull
+    private String id;
+    @NonNull
+    private String license;
+    @NonNull
+    private String state;
+    @NonNull
+    private String model;
+    @NonNull
+    private String color;
+    private LocalDateTime entryDate;
+    private LocalDateTime exitDate;
+    private Double bill;
 }
