58dff66614f9e6a1816e062aa95cae1b57cba84e
Paulo <deoliveira.pe@gmail.com>
2022-11-02 18:56:47 -0300

    config: Swagger configuration

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -3,6 +3,7 @@ package one.digitalinnovation.parking.service;
 import one.digitalinnovation.parking.model.Parking;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -12,8 +13,11 @@ public class ParkingService {
 
     static {
         var id = getUUID();
-        Parking parking = new Parking(id, "OMS-1111", "SC", "CELtA", "PRETO");
+        var id1 = getUUID();
+        Parking parking = new Parking(id, "OMS-1111", "SC", "CELTA", "PRETO");
+        Parking parking1 = new Parking(id1, "WAS-1234", "SP", "VW GOL", "VERMELHO");
         parkingMap.put(id, parking);
+        parkingMap.put(id1, parking1);
     }
 
     public List<Parking> findAll(){
@@ -22,5 +26,17 @@ public class ParkingService {
 
     private static String getUUID() {
         return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Parking findById(String id){
+        return parkingMap.get(id);
+    }
+
+    public Parking create(Parking parkingCreate) {
+        String uuid = getUUID();
+        parkingCreate.setId(uuid);
+        parkingCreate.setEntryDate(LocalDateTime.now());
+        parkingMap.put(uuid, parkingCreate);
+        return parkingCreate;
     }
 }
